73ee43dc8803918e3f9273aa165a2ce2b6fd7908
Isabella Janssen <ijanssen@redhat.com>
2026-04-03 13:32:37 -0400

    agent: support osImageStream for day-0 ABI install workflow for rhel-10 stream

--- pkg/types/validation/installconfig_test.go
+++ pkg/types/validation/installconfig_test.go
@@ -3016,6 +3016,16 @@ func TestValidateInstallConfig(t *testing.T) {
 			expectedError: "the Ingress capability is required",
 		},
 		{
+			name: "invalid OSImageStream value",
+			installConfig: func() *types.InstallConfig {
+				c := validInstallConfig()
+				c.FeatureSet = configv1.TechPreviewNoUpgrade
+				c.OSImageStream = "invalid"
+				return c
+			}(),
+			expectedError: "Unsupported OS Image Stream. Supported values are: rhel-9, rhel-10",
+		},
+		{
 			name: "invalid OSImageStream set",
 			installConfig: func() *types.InstallConfig {
 				c := validInstallConfig()
@@ -3032,16 +3042,6 @@ func TestValidateInstallConfig(t *testing.T) {
 					types.SCOS = old
 				}
 			},
-		},
-		{
-			name: "invalid OSImageStream set",
-			installConfig: func() *types.InstallConfig {
-				c := validInstallConfig()
-				c.FeatureSet = configv1.TechPreviewNoUpgrade
-				c.OSImageStream = "invalid"
-				return c
-			}(),
-			expectedError: "Unsupported OS Image Stream. Supported values are: rhel-9, rhel-10",
 		},
 	}
 	for _, tc := range cases {
